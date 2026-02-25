e8d088dacfbd7f5c0f8fefc9f7a7a033dddf1c0d
Harsh2004R <harshsharmaktm03@gmail.com>
2026-02-25 12:12:52 +0530

    Add Home.jsx

---
+++ src/mycomponents/Home.jsx
@@ -0,0 +1,18 @@
+
+import { Box } from '@chakra-ui/react'
+
+
+function Home() {
+  return (
+    <Box boxShadow="rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px"
+      w={"100%"} bg="#ffffff" h="100vh"
+      mt={{ base: "25px", md: "50px", lg: "60px" }}
+  
+
+    >
+
+    </Box>
+  )
+}
+
+export default Home
