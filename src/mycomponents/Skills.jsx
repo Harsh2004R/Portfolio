d598d862d2f05862b15971bfadfa77125d5705f1
Harsh2004R <harshsharmaktm03@gmail.com>
2026-03-09 21:21:33 +0530

    Skill.jsx in progress

---
+++ src/mycomponents/Skills.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Box, Text, Image, Flex, Center } from "@chakra-ui/react"
+
+function Skills() {
+    return (
+        <Box p={{ base: "6", md: "14", lg: "16" }} w="100%" minH="100vh" border="1px solid lime">
+            <Text color="#58595B" fontFamily={"bodytext3"} fontWeight={"600"} fontSize={{ base: "1.75rem", md: "2rem", lg: "2.25rem" }}>My Top Skills</Text>
+            <Text color="#58595B" fontFamily={"bodytext3"} fontSize={{ base: ".95rem", md: "1rem", lg: "1.125rem" }} >
+                what I Do
+            </Text>
+            <Center justifyContent={"left"} w="100%">
+                <Box mt=".75rem" bgColor="#e45447" boxSize={"7px"} borderRadius={"full"}>
+
+                </Box>
+                <Box mt=".75rem" bgColor="#e45447" w="140px" h="3px">
+
+                </Box>
+            </Center>
+
+
+            <Flex mt="2.25rem" w="100%" minH="70vh" bg="#F2F0EE">
+                <Box p={{ base: "2", md: "3", lg: "5" }} w="100%" h={{ base: "70vh", md: "70vh", lg: "70vh" }} border="1px solid red">
+                    {/* Content Box .................... */}
+
+
+                    <Text textAlign={"center"} mb="0.75rem" color="#58595B" fontFamily={"bodytext3"} fontWeight={"600"} fontSize={"1.125rem"}>BACKEND</Text>
+                    <Text lineHeight={"1.5rem"} textAlign={"center"} color="#58595B" fontFamily={"bodytext3"} fontSize={"1rem"}>
+                        I build scalable and maintainable backend applications using cutting-edge technologies like Node.js, Docker, Redis, Express, and MongoDB.
+                    </Text>
+
+                    <Text textAlign={"center"} mt="0.75rem" mb="0.75rem" color="#58595B" fontFamily={"bodytext3"} fontWeight={"600"} fontSize={"1.125rem"}>FRONTEND</Text>
+
+                </Box>
+                <Box display={{ base: "none", md: "flex", lg: "flex" }} w="100%" h={{ base: "70vh", md: "70vh", lg: "70vh" }} border="1px solid yellow">
+                    {/* Skill Image Box ...................................*/}
+                </Box>
+
+            </Flex>
+
+        </Box>
+    )
+}
+
+export default Skills
