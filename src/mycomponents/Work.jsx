b5ecd67cffaff83397f6f9c3ee0d19ece9bb419f
Harsh2004R <harshsharmaktm03@gmail.com>
2026-03-10 17:57:12 +0530

    Add new Component in Home

---
+++ src/mycomponents/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box, Text, Image, Flex, Grid, Center } from "@chakra-ui/react"
+
+
+
+
+function Work() {
+    return (
+        <Box w="100%" h="100vh" border={"2px solid grey"}>
+
+        </Box>
+    )
+}
+
+export default Work
